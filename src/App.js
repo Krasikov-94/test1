376e47db42eaa7feafaf53648967831fefc1cfc5
Ankr <krasikov-94@bk.ru>
2024-12-16 10:28:58 +0300

    Refactor imports and update package-lock.json; add footer to GeneralPage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { SecondPage } from './components/SecondPage';
-import { Menu } from './components/Menu';
-import { GeneralPage } from './components/OnePage/GeneralPage';
-import { Quote, ThirdPage } from './components/ThirdPage';
-import { useMediaQuery } from 'react-responsive';
+import { SecondPage } from "./components/SecondPage";
+import { Menu } from "./components/Menu";
+import { GeneralPage } from "./components/OnePage/GeneralPage";
+import { Quote, ThirdPage } from "./components/ThirdPage";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 440px)' });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 440px)" });
 
   return (
     <>
--- src/components/OnePage/GeneralPage/index.jsx
+++ src/components/OnePage/GeneralPage/index.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Header } from '../../Layout/Header';
-import { Description } from '../Description';
-import { Buttons } from '../Buttons';
-import { AddForm } from '../../Form';
-import { Modal } from '../../Modal';
-import Hamburger from 'hamburger-react';
-import styles from './GeneralPage.module.css';
-import stylesGam from './Slider.module.css';
+import React, { useState } from "react";
+import { Header } from "../../Layout/Header";
+import { Description } from "../Description";
+import { Buttons } from "../Buttons";
+import { AddForm } from "../../Form";
+import { Modal } from "../../Modal";
+import Hamburger from "hamburger-react";
+import styles from "./GeneralPage.module.css";
+import stylesGam from "./Slider.module.css";
 
 export const GeneralPage = ({ isTabletOrMobile }) => {
   const [isModalOpen, setModalState] = useState(false);
@@ -24,14 +24,14 @@ export const GeneralPage = ({ isTabletOrMobile }) => {
     <div className={isTabletOrMobile ? styles.wrapperPhone : styles.wrapper}>
       <div className={isTabletOrMobile ? styles.marPhone : styles.mar}>
         <Header isTabletOrMobile={isTabletOrMobile} />
-        <div id='modal' className={styles.modal}>
+        <div id="modal" className={styles.modal}>
           <Modal isOpen={isModalOpen} closeModal={closeModal}>
             <AddForm closeModal={closeModal} />
           </Modal>
         </div>
         {isTabletOrMobile && (
           <>
-            <Hamburger toggled={isOpen} toggle={setOpen} color='#e7e7e7' />
+            <Hamburger toggled={isOpen} toggle={setOpen} color="#e7e7e7" />
             {isOpen && (
               <div className={stylesGam.slider}>
                 <ul>
@@ -58,9 +58,16 @@ export const GeneralPage = ({ isTabletOrMobile }) => {
         )}
         <div className={styles.main}>
           <Description isTabletOrMobile={isTabletOrMobile} />
-          <Buttons handleOpenModal={handleOpenModal} isTabletOrMobile={isTabletOrMobile} />
+          <Buttons
+            handleOpenModal={handleOpenModal}
+            isTabletOrMobile={isTabletOrMobile}
+          />
         </div>
       </div>
+      <footer>
+        <p>© NAAM 2024</p>
+        <p>Политика конфиденциальности</p>
+      </footer>
     </div>
   );
 };
